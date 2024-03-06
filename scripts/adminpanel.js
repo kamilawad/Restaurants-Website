987a40a09d2b6cae417eea336c863b5b91cc1a27
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-06 20:43:08 +0200

    adminpanel js file

---
+++ scripts/adminpanel.js
@@ -0,0 +1,48 @@
+const addBtn = document.getElementById("add-user")
+const adminUser = document.querySelector(".admin-user-wrapper")
+const userInput = document.getElementById("add-input")
+
+
+const users = []
+const restaurants = [{
+
+}]
+let newUserContainer;
+
+function addUser(){
+    const user_name = userInput.value
+
+    if(user_name !== ""){
+        const user = {
+            username : user_name
+        }
+        users.push(user)
+  
+    
+
+    newUserContainer = document.createElement("div");
+    newUserContainer.className = 'users-container flex';
+    newUserContainer.innerHTML = `<p>Username:${user.username}</p>
+                                    <button class="delete-btn">Delete</button>`
+
+    adminUser.appendChild(newUserContainer)
+    userInput.value = ""
+
+
+    }
+    else{
+
+    }
+}
+adminUser.addEventListener("click", function(element){
+    if(element.target.tagName === "BUTTON"){
+        element.target.parentNode.remove()
+    }
+})
+
+
+
+addBtn.addEventListener("click",function(){
+    addUser()
+})
+
